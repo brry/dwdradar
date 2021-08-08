201a54b2f972a3190fe5c8af926719e079d17ce3
brry <berryboessenkool@hotmail.com>
2021-08-08 09:11:37 -0400

    readRadarFile: correctly reads YW files

--- R/readRadarFile.R
+++ R/readRadarFile.R
@@ -68,7 +68,7 @@ if(h$product=="RX") # WX,EX? - currently handled like SF # ToDo: Test
 dat.val <- dat.val*h$precision
 
 # convert into a matrix + give row and column names according to RADOLAN convention:
-if(h$product=="RW" | h$product=="SF" | h$product=="RQ")
+if(h$product=="RW" | h$product=="SF" | h$product=="RQ" | h$product=="YW")
   {
   dat.mat <- matrix(dat.val, ncol=DIM[2], byrow=TRUE) # ToDo: not sure about this
   dat.mat <- apply(dat.mat, 2, rev)
