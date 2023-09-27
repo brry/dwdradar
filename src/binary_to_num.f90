d81f5472bca876c56b8d0c6824590c9c763e0679
brry <berryboessenkool@hotmail.com>
2023-09-27 10:31:13 +0200

    line breaks or tabs or whatever

--- src/binary_to_num.f90
+++ src/binary_to_num.f90
@@ -19,24 +19,24 @@ subroutine binary_to_num(raw,Flength,numeric,Fna,Fclutter)
   integer,INTENT(in):: Flength,Fna,Fclutter
   integer(KIND=2),DIMENSION(Flength),INTENT(in)::raw
   integer,DIMENSION(Flength),INTENT(out)::numeric
-  
-  ! do cycle over all entries 
+
+  ! do cycle over all entries
   do i=1,Flength
-  
+
      numeric(i)=IBITS(raw(i),0,12)
-     
-     if(IBITS(raw(i),13,1)==1) THEN ! check for missing 
+
+     if(IBITS(raw(i),13,1)==1) THEN ! check for missing
         numeric(i)=Fna
      end if
-     
+
      if(IBITS(raw(i),14,1)==1) THEN ! check for negative
         numeric(i)=numeric(i)*(-1)
      end if
-     
+
      if(IBITS(raw(i),15,1)==1) THEN ! check for clutter
         numeric(i)=Fclutter
      end if
-     
+
   end do
 
 end subroutine binary_to_num
@@ -44,7 +44,7 @@ end subroutine binary_to_num
 
 
 
-! INFO: RX only one Byte 
+! INFO: RX only one Byte
 ! NA-Value: 250
 ! Clutter-Value: 249
 
@@ -54,20 +54,20 @@ subroutine binary_to_num_rx(raw,Flength,numeric,Fna,Fclutter)
   integer,INTENT(in):: Flength,Fna,Fclutter
   integer(KIND=1),DIMENSION(Flength),INTENT(in)::raw
   integer,DIMENSION(Flength),INTENT(out)::numeric
-  
-  ! do cycle over all entries 
-  do i=1,Flength 
+
+  ! do cycle over all entries
+  do i=1,Flength
 
      numeric(i)=IBITS(raw(i),0,7) + 128
-     
-     if(numeric(i)==250) THEN ! check for missing 
+
+     if(numeric(i)==250) THEN ! check for missing
         numeric(i)=Fna
      end if
-     
-     if(numeric(i)==249) THEN ! check for clutter 
+
+     if(numeric(i)==249) THEN ! check for clutter
         numeric(i)=Fclutter
      end if
-     
+
   end do
 
 end subroutine binary_to_num_rx
